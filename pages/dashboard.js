e556179814dcbe9b6bd4b85ab6b1b7eb2db51fc8
googooreads-sketch <googooreads@gmail.com>
2025-08-23 15:14:03 +0530

    Add files via upload

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,10 +1,21 @@
 import { Button } from "antd";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { getAuth } from 'firebase/auth';
 
 
 
 export default function Dashboard(){
+
+    const auth = getAuth();
+const user = auth.currentUser;
+
+if (user) {
+  console.log('UID:', user.uid);
+  console.log('Email:', user.email);
+  console.log('Display Name:', user.displayName);
+  console.log('Photo URL:', user.photoURL);
+}
 
     const router = useRouter();
 
